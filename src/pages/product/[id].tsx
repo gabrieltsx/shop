a43413d060162d9018bb94f3cb3452d2c10b26ed
Gabriel <gabrielhenrique.ls96@gmail.com>
2023-01-19 15:56:37 -0300

    add page product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,25 @@
-import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
-    const { query } = useRouter()
+  return (
+    <ProductContainer>
+      <ImageContainer>
 
-    return (
-      <h1>{JSON.stringify(query)}</h1>
-    )
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptatibus necessitatibus, a quo animi corrupti laudantium rem eius. Ullam veniam vero voluptas repellat officia placeat unde saepe tenetur esse a.</p>
+
+
+        <button>
+          Comprar agora
+        </button>
+
+      </ProductDetails>
+    </ProductContainer>
+  )
   }
   
